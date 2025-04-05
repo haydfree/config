9a3d178be871752a9296c0836239db4e93f8390a
Hayden Freeman <45716506+haydfree@users.noreply.github.com>
2025-04-05 14:22:19 -0500

    Update dwm_config.h

--- dwm_config.h
+++ dwm_config.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "IosevkaFixed:Regular:pixelsize=20:antialias=true:autohint=true" };
+static const char *fonts[]          = { "IosevkaNerdFont:Regular:pixelsize=16:antialias=true:autohint=true" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -26,10 +26,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "st-256color",        NULL,       NULL,       1 << 0,            0,           -1 },
-	{ "qutebrowser",        NULL,       NULL,       1 << 1,            0,           -1 },
-	{ "Anki",               NULL,       NULL,       1 << 2,            0,           -1 },
+	/* class                  instance    title       tags mask     isfloating   monitor */
+	{ "st-256color",           NULL,       NULL,       1 << 0,            0,           -1 },
+	{ "qutebrowser",           NULL,       NULL,       1 << 1,            0,           -1 },
+	{ "Anki",                  NULL,       NULL,       1 << 2,            0,           -1 },
+	{ "Zathura",               NULL,       NULL,       1 << 3,            0,           -1 },
 };
 
 /* layout(s) */
@@ -60,9 +61,9 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *upvol[] = { "/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%", NULL };
-static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%", NULL };
-static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute", "@DEFAULT_SINK@", "toggle", NULL };
+static const char *upvol[] =   { "/usr/bin/amixer", "set", "Master", "5%+"};
+static const char *downvol[] = { "/usr/bin/amixer", "set", "Master", "5%-"};
+static const char *mutevol[] = { "/home/ned/projects/bash/toggle_mute.sh", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
